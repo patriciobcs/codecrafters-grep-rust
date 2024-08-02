47d3b354ee335d420737f625afff6f61bcc8ad96
Patricio Calderon <patricio.calderon@sansano.usm.cl>
2024-08-02 23:05:51 +0200

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
+    if pattern.chars().count() > 0 {
         return input_line.contains(pattern);
     } else {
         panic!("Unhandled pattern: {}", pattern)
